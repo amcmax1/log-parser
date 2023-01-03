9e5fd824ec3f84c4b76e6a0b03825f157418ff81
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2023-01-03 16:34:34 +0200

    (feat:) text processing scaffolds WIP

--- app/src/app.ts
+++ app/src/app.ts
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "../.env" });
 import http from "http";
 import Pino from "pino";
 import { AddressInfo } from "net";
-
+import { EnumType } from "typescript";
 const LISTEN_NET = "0.0.0.0";
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 9883;
 
@@ -12,25 +12,114 @@ const logger: Pino.Logger = Pino({
 	timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
 });
 
+const fs = require("fs");
+const es = require("event-stream");
+
+// export as utility
+function parseline(line: any) {
+	const data = line.split("- -");
+	return data;
+}
+
 const server = http.createServer((req, res) => {
 	console.log("OK");
-	// separate service that will take a large file and split into chunks, by line and dimension
-	// each chunk then sent as a separate request to this parse-model-service
-	// use Proxy to multiple instances of this service and limiter guards
-	// db adapter and models
 });
 
 server.on("error", (e) => {
 	logger.error({ error: e }, "Unhandled error on server");
 });
 
+// export as utility with parameters for required metrics as enum or custom type
+async function processApacheLogEntry(
+	logfilename: String,
+	metrics?: Array<EnumType>
+) {
+	let lineN = 0;
+	const ipsArray: Array<String> = [];
+	const userAgents: Array<String> = [];
+
+	console.time("ReadStream benchmark");
+	const s = fs
+		.createReadStream(logfilename)
+		.pipe(es.split())
+		.pipe(
+			es
+				.mapSync(function (line: String) {
+					s.pause();
+
+					let parsedLine: Array<String> = parseline(line);
+
+					ipsArray.push(parsedLine[0]);
+					userAgents.push(parsedLine[1]);
+					lineN += 1;
+
+					s.resume();
+				})
+				.on("error", function (err: any) {
+					console.log("Error while reading file.", err);
+				})
+				.on("end", function () {
+					console.log("IPs: ", ipsArray.length);
+					console.log("User Agents: ", userAgents.length);
+					console.log("Read entire file.");
+
+					// resolve on condition that the metric is given as an argument in processApacheLogEntry()
+					const countryReport = new Promise((resolve, reject) => {
+						resolve(geoLocateIPs(ipsArray));
+					});
+					// resolve on condition that the metric is given as an argument in processApacheLogEntry()
+					const systemReport = new Promise((resolve, reject) => {
+						resolve(identifySystems(userAgents));
+					});
+					// resolve on condition that the metric is given as an argument in processApacheLogEntry()
+					const browserReport = new Promise((resolve, reject) => {
+						resolve(identifyBrowsers(userAgents));
+					});
+
+					Promise.all([countryReport, systemReport, browserReport]).then(
+						(values) => {
+							// send to main() subscriber
+							console.log("FINAL LOG STATS REPORT:", values);
+						}
+					);
+				})
+		);
+	console.timeEnd("ReadStream benchmark");
+}
+
+// export as service and implement statistics/API requests/calculations with a type model
+async function geoLocateIPs(ips: Array<String>) {
+	setTimeout(() => {
+		console.log("locations identified", ips.length);
+	}, 1000);
+
+	return `cool results 1: ${ips.length}`;
+}
+
+// export as service and implement statistics/API requests/calculations with a type model
+function identifySystems(useragents: Array<String>) {
+	setTimeout(() => {
+		console.log("systems identified", useragents.length);
+	}, 1000);
+	return `cool results 2: ${useragents.length}`;
+}
+
+// export as service and implement statistics/API requests/calculations with a type model
+function identifyBrowsers(useragents: Array<String>) {
+	setTimeout(() => {
+		console.log("browsers identified", useragents.length);
+	}, 1000);
+	return `cool results 3: ${useragents.length}`;
+}
+
+async function main() {
+	processApacheLogEntry("../raw/apache_log.txt");
+}
+
 const app = server.listen(PORT, LISTEN_NET, async () => {
 	const { address, port } = app.address() as AddressInfo;
 	try {
-		setTimeout(() => {
-			console.log("Delay.");
-		}, 1000);
-
+		main();
 		logger.info("Server running delay");
 	} catch (error) {
 		logger.info({ error }, "Server not running");
